34a9eadef7648d47b59b29d467f21499ccffe8aa
Thomas Hobson <iamthomasnz@gmail.com>
2020-03-08 17:16:07 +1300

    Disallow conflicting links

--- src/Datastore/MemoryDatastore.ts
+++ src/Datastore/MemoryDatastore.ts
@@ -7,18 +7,28 @@ export class MemoryDatastore implements IDatastore {
 
     addLink(link: Link): Promise<void> {
         return new Promise((resolve, reject) => {
+            
+
+            let existing = MemoryDatastore.db.find((v) => v.shortened == link.shortened || v.id == link.id)
+            if (existing) reject(`Conflicting link (id: ${existing.id})`)
+            
             MemoryDatastore.db.push(link)
+
             resolve()
         })
     }
     getLinkByShortened(shortened: string): Promise<Link> {
         return new Promise((resolve, reject) => {
-            resolve(MemoryDatastore.db.find((v) => v.shortened == shortened))
+            let link = MemoryDatastore.db.find((v) => v.shortened == shortened)
+            if (link) resolve(link)
+            else reject("Link not found")
         })
     }
     getLinkById(id: number): Promise<Link> {
         return new Promise((resolve, reject) => {
-            resolve(MemoryDatastore.db.find((v) => v.id == id))
+            let link = MemoryDatastore.db.find((v) => v.id == id)
+            if (link) resolve(link)
+            else reject("Link not found")
         })
     }
     getLinks(): Promise<Array<Link>> {
